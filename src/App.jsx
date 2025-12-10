de7271ee6952cb8efe639cb51346733e46727b68
Petro Kustov <petr.kustov@skyrora.com>
2025-12-10 11:25:10 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -37,6 +37,7 @@ export const App = () => {
         <tbody>
           {goods.map(good => (
             <tr
+              key={good}
               data-cy="Good"
               className={
                 selectedGood === good
